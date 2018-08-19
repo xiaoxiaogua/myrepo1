58112d9b48028bb11475eda544d9e5124f6a29fc
VictorZhang <zhangshuai_work@outlook.com>
2018-08-19 15:01:25 +0800

    Modify Hello.java and Add Demo.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,5 @@
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello git");
-    System.out.println("www.java.com");
+    System.out.println("This is hello.java output");
   }
 }
